53b4e6b4971ae5682449c6aa5796b3e03ef61e03
Bruno Cruz <bru.cruz@gmail.com>
2023-06-08 10:01:59 -0300

    chore: remove unnecessary comments

--- packages/functions/src/fetchProductFromUrl.ts
+++ packages/functions/src/fetchProductFromUrl.ts
@@ -4,13 +4,9 @@ import { ProductInsertion } from "./types/ProductInsertion";
 export const supportedDomains = ["oqvestir.com.br", "shop2gether.com.br"];
 
 export async function fetchProductFromUrl(url: string) {
-  // get url domain
   const domain = new URL(url).hostname;
-
-  // check if domain has www and remove it
   const domainWithoutWWW = domain.replace("www.", "");
 
-  // check if domain is supported
   if (!supportedDomains.includes(domainWithoutWWW)) {
     throw new Error("Unsupported domain");
   }
@@ -19,11 +15,9 @@ export async function fetchProductFromUrl(url: string) {
 
   switch (domainWithoutWWW) {
     case "oqvestir.com.br":
-      // fetch product from oqvestir
       product = await scraper.oqvestir(url, domain, domainWithoutWWW);
       break;
     case "shop2gether.com.br":
-      // fetch product from oqvestir
       product = await scraper.shop2gether(url, domain, domainWithoutWWW);
       break;
     default:
